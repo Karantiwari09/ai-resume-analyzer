ce28a9de154ff740d3728532aa64799d6509890b
Karantiwari09 <karantiwri740@gmail.com>
2025-06-27 12:59:49 +0530

    Fix: SpaCy model download added via try-except

--- utils/ner_extractor.py
+++ utils/ner_extractor.py
@@ -1,13 +1,16 @@
 import spacy
+from spacy.cli import download
 
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 def extract_entities(resume_text):
     doc = nlp(resume_text)
     entities = {"PERSON": [], "ORG": [], "GPE": [], "EMAIL": [], "DATE": [], "SKILL": []}
-
     for ent in doc.ents:
         if ent.label_ in entities:
             entities[ent.label_].append(ent.text)
-    
     return entities
