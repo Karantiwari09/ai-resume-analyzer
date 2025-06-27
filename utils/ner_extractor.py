31e8a9bf2ac619b21b442d0f037ba61811dd9d16
Karantiwari09 <karantiwri740@gmail.com>
2025-06-27 18:22:15 +0530

    Final working

--- utils/ner_extractor.py
+++ utils/ner_extractor.py
@@ -6,11 +6,3 @@ try:
 except OSError:
     download("en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
-
-def extract_entities(resume_text):
-    doc = nlp(resume_text)
-    entities = {"PERSON": [], "ORG": [], "GPE": [], "EMAIL": [], "DATE": [], "SKILL": []}
-    for ent in doc.ents:
-        if ent.label_ in entities:
-            entities[ent.label_].append(ent.text)
-    return entities
